3d7c213cb90409e890b368d7d7f7568275b99829
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-16 23:08:36 -0200

    Estrutura inicial do projeto.

---
+++ ManagementApp/src/GL.ManagementApp.Presentation.UI.Management/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GL.ManagementApp.Presentation.UI.Management
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ManagementApp/src/GL.ManagementApp.Presentation.UI.Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GL.ManagementApp.Presentation.UI.Management.Startup))]
+namespace GL.ManagementApp.Presentation.UI.Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
