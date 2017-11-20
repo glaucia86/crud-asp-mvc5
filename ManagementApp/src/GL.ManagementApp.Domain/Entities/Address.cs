3ed825d14d8062e1b58784279db11c0b576f44f4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-20 19:23:00 -0200

    Criação da classe 'Address'

---
+++ ManagementApp/src/GL.ManagementApp.Domain/Entities/Address.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GL.ManagementApp.Domain.Entities
+{
+    public class Address
+    {
+        /// <summary>
+        /// Id do Endereço
+        /// </summary>
+        public Guid AddressId { get; set; }
+
+        /// <summary>
+        /// Endereço
+        /// </summary>
+        public string PublicPlace { get; set; }
+
+        /// <summary>
+        /// Número do Endereço
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// Complemento do Endereço
+        /// </summary>
+        public string AddressComplement { get; set; }
+
+        /// <summary>
+        /// CEP
+        /// </summary>
+        public string ZipCode { get; set; }
+
+        /// <summary>
+        /// Cidade
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Estado
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// País
+        /// </summary>
+        public string Country { get; set; }
+    }
+}
