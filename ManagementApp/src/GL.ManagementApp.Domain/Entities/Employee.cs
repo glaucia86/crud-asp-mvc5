5d09da233cfc8d05b354edae9ab6dce18d024150
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-20 19:07:18 -0200

    Criação da classe 'Employee'

--- ManagementApp/src/GL.ManagementApp.Domain/Entities/Employee.cs
+++ ManagementApp/src/GL.ManagementApp.Domain/Entities/Employee.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GL.ManagementApp.Domain.Entities
 {
-    class Employee
+    public class Employee
     {
+        /// <summary>
+        /// Id do Empregado
+        /// </summary>
+        public Guid EmployeeId { get; set; }
+
+        /// <summary>
+        /// Primeiro Nome
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Sobrenome
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Data de Criação do Cadastro do Empregado
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Data de Nascimento
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+
+        /// <summary>
+        /// Ativo (se o empregado está ativo)
+        /// </summary>
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Cargo Ocupado
+        /// </summary>
+        public string Occupation { get; set; }
+
     }
 }
