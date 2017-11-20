022d28dfd515e69931640a34e52ce7808a7d0782
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-20 21:05:40 -0200

    Criação do método 'modelBuilder' para a configuração das tables via ORM

--- ManagementApp/src/GL.ManagementApp.Infra.Data/Context/ManagementAppContext.cs
+++ ManagementApp/src/GL.ManagementApp.Infra.Data/Context/ManagementAppContext.cs
@@ -1,5 +1,6 @@
 ﻿using GL.ManagementApp.Domain.Entities;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace GL.ManagementApp.Infra.Data.Context
 {
@@ -14,5 +15,37 @@ namespace GL.ManagementApp.Infra.Data.Context
         public DbSet<Employee> Employees { get; set; }
 
         public DbSet<Address> Addresses { get; set; }
+
+        /// <summary>
+        /// Método responsável por criar modelos das classes do banco
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Aqui ele vai remover a pluralização das tabelas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //Aqui ele evitará de remover a tables de maneira cascada
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //Aqui estou indicando qual propriedade é a Chave Primária
+            //Que nesse caso são as propriedades com terminação "Id":
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            //Aqui estou configurando que, todas as propriedades do tipo 'string' sejam do tipo
+            //'varchar'
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            //Aqui estou configurando que, todas as propriedades do tipo 'string
+            //tenham tamanho máximo de 100
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
